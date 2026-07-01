c0b2f7b3ef7a1a617edceb04e6928bd1228fd06b
jaime_r_pereira <jaime_r_pereira@estudante.sesisenai.org.br>
2026-07-01 09:57:16 -0300

    add- tipos dos cafés e organização do código

--- cafes.java
+++ cafes.java
@@ -1,7 +1,17 @@
 public class Main {
-  public static void main(String[] args) {
-    String name = "cafeteiro o que seria hoje?";
-    System.out.println("Bem vindo " + name);
-  }
+    public static void main(String[] args) {
+        String name = "cafeteiro Mateus o que seria hoje?";
+        System.out.println("Bem vindo! Respondendo à pergunta: '" + name + "'");
+        System.out.println("Hoje, o profissional especialista no preparo de cafés é conhecido como Barista e o nome dele é Jaime.");
+        System.out.println("----------------------------------------");
+        
+     
+        String[] tiposDeCafe = {"espresso", "duplo", "coado", "simples", "cappuccino"};
+        
+        System.out.println("Tipos de café disponíveis:");
+        
+        for (String cafe : tiposDeCafe) {
+            System.out.println("- " + cafe);
+        }
+    }
 }
-
